c6fa056ae86fb5e841f9db8f672655f90bafb9b5
kuddlefishbwaa <72284217+kuddlefishbwaa@users.noreply.github.com>
2024-10-10 13:55:07 -0600

    Refactoring

---
+++ CS3710_Docker Image_Files/WebApp/test/models/student_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class StudentTest < ActiveSupport::TestCase
+
+  test "Should rise error when the email is not unique." do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "TEST", last_name: "TEST", school_email: "jsmith33@msudenver.edu", major: "TEST", graduation_date: "2024-09-17")
+    end
+  end
+  
+  test "Should rise error when the email is not ending in @msudenver.edu" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "TEST", last_name: "TEST", school_email: "jsmith@gmail.edu", major: "TEST", graduation_date: "2024-09-17")
+    end
+  end
+
+  test "Should rise error when any field is empty." do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "TEST", last_name: "TEST", school_email: "jsmith222@gmail.edu", major: "TEST", graduation_date: "2024-09-17")
+    end
+  end
+
+
+end
+
