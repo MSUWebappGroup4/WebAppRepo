cce6b7136f5f01e2d7e6e7e1fe8896e483a33b74
kuddlefishbwaa <72284217+kuddlefishbwaa@users.noreply.github.com>
2024-10-22 12:59:06 -0600

    installing devise

--- CS3710_Docker Image_Files/WebApp/app/models/student.rb
+++ CS3710_Docker Image_Files/WebApp/app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_one_attached :profile_picture, dependent: :purge_later
     validates :first_name, presence: true
     validates :last_name, presence: true
